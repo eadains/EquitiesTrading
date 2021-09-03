7a910598036353141777d95ba6efabc5877403d3
Erik <eadains@gmail.com>
2021-09-02 19:57:59 -0500

    feature creation finished

--- queries.sql
+++ queries.sql
@@ -1,57 +1,27 @@
 SELECT
-    datekey,
-    ticker,
-    pe
+    ticker
 FROM
-    fundamentals
+    tickers
 WHERE
-    dimension = 'ARQ'
-ORDER BY
-    date DESC;
-
-SELECT
-    date,
-    ticker,
-    closeadj
-FROM
-    prices
-WHERE
-    date IN (
-        SELECT
-            MAX(date)
-        FROM
-            prices
-        WHERE
-            frequency = 'DAILY'
-        GROUP BY
-            EXTRACT(
-                MONTH
-                FROM
-                    date
-            ),
-            EXTRACT(
-                YEAR
-                FROM
-                    date
-            )
-    )
-ORDER BY
-    date ASC;
-
-SELECT
-    *
-FROM
-    prices
-WHERE
-    ticker IN (
-        SELECT
-            *
-        FROM
-            tickers
-        WHERE
-            (
-                category = 'ETF'
-                AND isdelisted = FALSE
-                AND firstpricedate <= '2007-12-31' :: date
-            )
+    (
+        EXTRACT(
+            YEAR
+            FROM
+                lastpricedate
+        ) - EXTRACT(
+            YEAR
+            FROM
+                firstpricedate
+        ) >= 5
+        AND EXTRACT(
+            YEAR
+            FROM
+                lastquarter
+        ) - EXTRACT(
+            YEAR
+            FROM
+                firstquarter
+        ) >= 5
+        AND category = 'Domestic Common Stock'
+        AND currency = 'USD'
     );
